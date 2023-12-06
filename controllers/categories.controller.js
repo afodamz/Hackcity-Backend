ce73fa090c0d87cb8e4d4b6379466f3490b0c997
afodamz <afodamz07@gmail.com>
2023-12-06 14:09:48 +0100

    Create, Read, Update and delete categories

---
+++ controllers/categories.controller.js
@@ -0,0 +1,229 @@
+const Sequelize = require("sequelize");
+const _ = require('lodash');
+const db = require('./../models')
+const constants = require('./../utils/constants');
+const CategoryResponseDto = require('../dtos/responses/category.dto');
+const CategoryRequestDto = require('./../dtos/requests/category.dto');
+const AppResponseDto = require('./../dtos/responses/app_response.dto')
+
+const Categories = db.categories;
+
+const Op = Sequelize.Op;
+
+exports.findAll = async function (req, res, next) {
+    try {
+        const {status, search, title} = req.query;
+        const limit = Number(req.query?.limit || constants.DEFAULT_TAKE);
+        const offset = Number(req.query?.offset || constants.DEFAULT_SKIP);
+        let query = {};
+        const subQuery = []
+
+        if (title) {
+            subQuery.push({
+                title: {
+                    [Op.eq]: title
+                }
+            })
+        }
+        if (search) {
+            subQuery.push({
+                [Op.or]: [
+                    {
+                        title: {
+                            [Op.like]: `%${search}%`
+                        },
+                    },
+                    {
+                        content: {
+                            [Op.like]: search
+                        },
+                    },
+                ]
+            })
+        }
+        subQuery.push({
+            isDeleted: {
+                [Op.eq]: false
+            }
+        })
+        query = {
+            [Op.and]: subQuery,
+        }
+
+        const categories = await Categories.findAndCountAll({
+            where: query,
+            limit: limit,
+            offset: offset,
+            order: [
+                ["createdAt", "DESC"]
+            ],
+        });
+        const {rows, count} = categories
+        return res.json(CategoryResponseDto.buildPagedList(rows, rows.length, limit, offset, count));
+    } catch (error) {
+        return res.json(AppResponseDto.buildSuccessWithMessages(error.message));
+    }
+};
+
+exports.find = async function (req, res, next) {
+    try {
+        const {search, title} = req.query;
+        const limit = Number(req.query?.limit || constants.DEFAULT_TAKE);
+        const offset = Number(req.query?.offset || constants.DEFAULT_SKIP);
+        let query = {};
+        const subQuery = []
+        if (title) {
+            subQuery.push({
+                title: {
+                    [Op.eq]: title
+                }
+            })
+        }
+        if (search) {
+            subQuery.push({
+                title: {
+                    [Op.like]: search
+                },
+            })
+        }
+        subQuery.push({
+            isDeleted: {
+                [Op.eq]: false
+            },
+        })
+        query = {
+            [Op.and]: subQuery,
+        }
+
+        const categories = await Categories.findAndCountAll({
+            where: query,
+            limit: limit,
+            offset: offset,
+            order: [
+                ["createdAt", "DESC"]
+            ],
+        });
+        const {rows, count} = categories
+        return res.json(CategoryResponseDto.buildPagedList(rows, rows.length, limit, offset, count));
+    } catch (error) {
+        return res.json(AppResponseDto.buildSuccessWithMessages(error.message));
+    }
+};
+
+exports.create = async function (req, res, next) {
+    try {
+        const resultBinding = await PostRequestDto.create(req.body);
+        if (!_.isEmpty(resultBinding.errors)) {
+            return res.json(AppResponseDto.buildWithErrorMessages(resultBinding.errors));
+        }
+        const title = resultBinding.validatedData.title;
+        const description = req.body.description;
+
+        // let foundCategories = await Categories.findOne({
+        //     where: {
+        //         id: categories,
+        //         isDeleted: false,
+        //     },
+        // });
+        // if (!foundCategories) return res.json(AppResponseDto.buildWithErrorMessages('Preacher not found'));
+        const savedPost = await Categories.create({
+            title,
+            description,
+        });
+
+        const foundData = await Categories.findOne({
+            where: {
+                id: savedPost.dataValues.id,
+                isDeleted: false,
+            },
+        });
+        return res.json(CategoryResponseDto.buildDetails(foundData.dataValues, false, false));
+
+    } catch (error) {
+        return res.json(AppResponseDto.buildWithErrorMessages(error.message));
+    }
+};
+
+exports.update = async function (req, res, ext) {
+    try {
+        if (!req.params.id) return res.json(AppResponseDto.buildWithErrorMessages('Category with id not found ' + err));
+        const data = req.body;
+        const id = req.params.id;
+        const foundData = await Posts.findOne({
+            where: {
+                id,
+                isDeleted: false,
+            },
+        });
+        if (!foundData.dataValues) return res.json(AppResponseDto.buildWithErrorMessages('Post not found'));
+        const model = foundData.dataValues;
+        if (data.title) model.title = data.title;
+        if (data.description) model.description = data.description;
+
+        await Categories.update(
+            model,
+            {
+                where: {
+                    id: id,
+                },
+                returning: true,
+            }
+        );
+        const updatedPost = await Categories.findOne({
+            where: {
+                id,
+                isDeleted: false,
+            },
+        });
+        // console.log('updatedPost', updatedPost)
+        const updatedData = updatedPost.dataValues;
+        // const updatedData = {...foundData.dataValues, ...model}
+        return res.json(categoryResponseDto.buildDetails(updatedData))
+    } catch (error) {
+        return res.json(AppResponseDto.buildWithErrorMessages('Error ' + error));
+    }
+};
+
+exports.delete = async function (req, res, next) {
+    try {
+        if (!req.params.id) return res.json(AppResponseDto.buildWithErrorMessages('Category with id not found '));
+        const id = req.params.id;
+        const foundData = await Categories.findOne({
+            where: {
+                id: req.params.id,
+                isDeleted: false,
+            },
+        });
+        if (!foundData) return res.json(AppResponseDto.buildWithErrorMessages('Category not found'));
+        await Categories.update(
+            {isDeleted: true},
+            {
+                where: {
+                    id: id,
+                },
+            }
+        );
+        return res.json(AppResponseDto.buildSuccessWithMessages('Category removed successfully'));
+    } catch (error) {
+        return res.json(AppResponseDto.buildWithErrorMessages('Error ' + error));
+    }
+};
+
+exports.findOne = async function (req, res, next) {
+    try {
+        if (!req.params.id) return res.json(AppResponseDto.buildWithErrorMessages('Category with id not found '));
+        const foundData = await Categories.findOne({
+            where: {
+                id: req.params.id,
+                isDeleted: false,
+            },
+        });
+        if (foundData) {
+            return res.json(CategoryResponseDto.buildDetails(foundData))
+        } else {
+            return res.json(AppResponseDto.buildWithErrorMessages('Category not found'));
+        }
+    } catch (error) {
+        return res.json(AppResponseDto.buildWithErrorMessages('Error ' + error));
+    }
+};
--- dtos/requests/category.dto.js
+++ dtos/requests/category.dto.js
@@ -1,5 +1,4 @@
 const sanitizeInput = require('./../../utils/sanitize').sanitizeInput;
-const bcrypt = require('bcryptjs')
 
 exports.create = (body) => {
     const resultBinding = {
@@ -12,30 +11,10 @@ exports.create = (body) => {
     else
         resultBinding.validatedData.title = sanitizeInput(body.title);
 
-    if (!body.preacher || body.preacher.trim() === '')
-        resultBinding.errors.preacher = 'description is required';
-    else
-        resultBinding.validatedData.preacher = sanitizeInput(body.preacher);
-
-    if (!body.scripture || body.scripture.trim() === '')
-        resultBinding.errors.scripture = 'status is required';
-    else
-        resultBinding.validatedData.scripture = sanitizeInput(body.scripture);
-
     if (!body.description || body.description.trim() === '')
         resultBinding.errors.description = 'description is required';
     else
         resultBinding.validatedData.description = sanitizeInput(body.description);
-
-    if (!body.audio || body.audio.trim() === '')
-        resultBinding.errors.audio = 'audio is required';
-    else
-        resultBinding.validatedData.audio = sanitizeInput(body.audio);
-
-    if (!body.image || body.image.trim() === '')
-        resultBinding.errors.image = 'image is required';
-    else
-        resultBinding.validatedData.image = sanitizeInput(body.image);
 
     return resultBinding;
 };
