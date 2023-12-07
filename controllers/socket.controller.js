e0dfa6f6bffbfc06a8df57e63e34659b75ae95b5
afodamz <afodamz07@gmail.com>
2023-12-07 08:07:44 +0100

    admin realtime with socket based on module

--- controllers/socket.controller.js
+++ controllers/socket.controller.js
@@ -1,9 +1,9 @@
 
 const io = require('socket.io')(http);
 
-exports.emitMessageToAdmin = function (EVENT, module, data) {
+exports.emitMessageToAdmin = function (UPDATE_EVENT, module, data) {
     try {
-        io.emit(EVENT, { module, data });
+        io.emit(UPDATE_EVENT, { module, data });
     } catch (error) {
         console.log('error emitting message')
     }
